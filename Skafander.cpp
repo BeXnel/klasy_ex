c66a6a4d1877c7254639556e19f3beabeba237d7
BeXnel <55487209+BeXnel@users.noreply.github.com>
2020-02-25 23:09:16 +0100

    Update Skafander.cpp

--- Skafander.cpp
+++ Skafander.cpp
@@ -1,34 +1,70 @@
 #include "Skafander.h"
-#include <iostream>
 #include <string>
-
 using namespace std;
 
-namespace world
+//konstruktor
+Skafander::Skafander() :
+    m_colour{ "white" },
+    m_material{ "plastic" },
+    m_size{ "S" },
+    m_weight{8},//kilograms
+    m_length{ 60},//centimeters
+    m_width{ 50 },//centimeters
+    m_purpouse{ "chemical" }
 {
-	//Konstruktor domyúlny
-	Skafander::Skafander():
-		m_weight{ 3.0f }, //kilogramy
-		m_kolor {},
-		m_length {15.0f}, //centymetry
-		m_purpose {},
 
+}
 
-		
+//destruktor
+Skafander::~Skafander()
+{
+    m_colour = { 0 };
+    m_material = { 0 };
+    m_size = { 0 };
+    m_weight = { 0 };//kilograms
+    m_length = { 0 };//centimeters
+    m_width = { 0 };//centimeters 
+    m_purpouse = { 0 };
+}
 
+//Metoda ustawia kolor skafandru
+void Skafander::setColour(string colour)
+{
+    m_colour = colour;
+}
 
+//Metoda ustawia rozmiar skafandru
+void Skafander::setSize(char size)
+{
+    m_size = size;
+}
 
+//Metoda ustawia materia≈Ç, z jakiego jest wykonany szlafrok
+void Skafander::setMaterial(string material)
+{
+    m_material = material;
+}
 
+  //Metoda ustawia przeznaczenie skafandra
+void Skafander::setPurpouse(string purpouse)
+{
+    m_purpouse = purpouse;
+}
 
+//Metoda ustawia szerokosc skafandra
+void setWidth(float width)
+{
+    m_width = width;
+}
 
+//Metoda ustawia dlugosc skafandra
+void setLength(float length)
+{
+    m_length = length;
+}
 
-
-
-
-
-
-
-
-
-
-}//world
+//Metoda ustawia wage skafandru
+void setWeight(float weight)
+{
+    m_weight = weight;
+}
